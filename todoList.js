bb75ea861bb802d023060c4fd47c42470141d011
ABENEZER-SELESHI <ebenezerseleshi@gmail.com>
2025-07-10 14:06:46 -0700

    Added local caching to store tasks even after reload

--- todoList.js
+++ todoList.js
@@ -1,5 +1,8 @@
-const taskList = []
-const completedTaskList = []
+const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+displayTask();
+const completedTaskList = JSON.parse(localStorage.getItem('completed-tasks')) || [];
+displayCompletedTasks()
+
 
 
 function addNewTask() {
@@ -9,6 +12,7 @@ function addNewTask() {
     if (task === '') return;
 
     taskList.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskList))
     input.value = '';
     displayTask();
 }
@@ -31,7 +35,10 @@ function displayTask() {
         completeBtn.className = 'complete-btn';
         completeBtn.addEventListener('click', () => {
             completedTaskList.push(t)
+            localStorage.setItem('completed-tasks', JSON.stringify(completedTaskList))
             removeTask(index);
+            displayCompletedTasks()
+
         })
 
         // Edit button
@@ -42,6 +49,7 @@ function displayTask() {
             const newTask = prompt('Edit task:', t);
             if (newTask !== null && newTask.trim() !== '') {
                 taskList[index] = newTask.trim();
+                localStorage.setItem('tasks', JSON.stringify(taskList))
                 displayTask();
             }
         });
@@ -63,8 +71,41 @@ function displayTask() {
     });
 }
 
+function displayCompletedTasks() {
+    const completedTaskListDiv = document.getElementById('completedTaskListDisplay');
+    completedTaskListDiv.innerHTML = '';
+
+    completedTaskList.forEach((t, index) => {
+        const taskDiv = document.createElement('div');
+        taskDiv.className = 'completed-item';
+
+        const taskText = document.createElement('span');
+        taskText.textContent = `${t}`;
+        taskText.className = 'task-text';
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = 'delete-btn'
+        deleteBtn.textContent = 'Delete'
+        deleteBtn.addEventListener('click', () => {
+            removeCompletedTask(index)
+        })
+
+        taskDiv.appendChild(taskText)
+        taskDiv.appendChild(deleteBtn)
+
+        completedTaskListDiv.appendChild(taskDiv)
+    })
+}
+
 
 function removeTask(ind) {
     taskList.splice(ind,1);
+    localStorage.setItem('tasks', JSON.stringify(taskList))
     displayTask();
+}
+
+function removeCompletedTask(ind) {
+    completedTaskList.splice(ind, 1);
+    localStorage.setItem('completed-tasks', JSON.stringify(completedTaskList));
+    displayCompletedTasks()
 }
